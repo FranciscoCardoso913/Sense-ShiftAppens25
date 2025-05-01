89c3d2d2e269774ad1eca4455f815909359a5a92
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2025-05-01 23:26:28 +0100

    Main page basic design

--- sense/lib/MainPage.dart
+++ sense/lib/MainPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class MainPage extends StatefulWidget {
   const MainPage({super.key, required this.title});
   final String title;
@@ -18,20 +19,40 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+   
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              flex: 3,
+              child: Align(
+    alignment: Alignment.center,
+    child: Image.asset('assets/logo.png')
+  ),
+            ),
+              Expanded(
+              flex: 6,
+              child: const Text('You have pushed the button this many times:'),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Divider(),
+                  Row(
+                    mainAxisAlignment:
+                        MainAxisAlignment.center, // Centers the buttons
+                    children: [
+                      IconButton(onPressed: () {}, icon: Icon(Icons.volunteer_activism_outlined), iconSize: 30,),
+                      SizedBox(width: 30), // Adds space between the buttons
+                      IconButton(onPressed: () {}, icon: Icon(Icons.help_outline_rounded), iconSize: 30,),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -40,7 +61,7 @@ class _MainPageState extends State<MainPage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
