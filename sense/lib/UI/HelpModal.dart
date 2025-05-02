56c79cfc642eb1452bdd2d11bed9f6a1a8652b10
Madalena Ye <mads1040@gmail.com>
2025-05-02 16:02:56 +0100

    help modal done

--- sense/lib/UI/HelpModal.dart
+++ sense/lib/UI/HelpModal.dart
@@ -46,14 +46,52 @@ class HelpModal extends StatelessWidget {
                 child: Image.asset('assets/Listen.png', height: 25),
               ),
               Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "Start by finding the sound your phone is hearing. Click the flower center",
-                  style: TextTheme.of(
-                    context,
-                  ).bodyMedium?.copyWith(fontFamily: "Poppins"),
-                ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/help_modal/1st_row.png'),
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      "Start by finding the sound your phone is hearing. Click the flower center",
+                      style: TextTheme.of(
+                        context,
+                      ).bodyMedium?.copyWith(fontFamily: "Poppins"),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Text(
+                      "When the flower is all colored, the AI model finished executing! You’ll see its result bellow the flower",
+                      style: TextTheme.of(
+                        context,
+                      ).bodyMedium?.copyWith(fontFamily: "Poppins"),
+                    ),
+                  ),
+                  Image.asset('assets/help_modal/2nd_row.png'),
+                  SizedBox(width: 12),
+                ],
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/help_modal/3rd_row.png'),
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      "Then you can select if you want to locate the source of the sound or detect another one",
+                      style: TextTheme.of(
+                        context,
+                      ).bodyMedium?.copyWith(fontFamily: "Poppins"),
+                    ),
+                  ),
+                ],
               ),
               Padding(padding: EdgeInsets.symmetric(vertical: 12)),
               Align(
@@ -61,10 +99,58 @@ class HelpModal extends StatelessWidget {
                 child: Image.asset('assets/Locate.png', height: 25),
               ),
               Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/help_modal/4th_row.png'),
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      "The flower is gray again. Now you need to keep the phone flat and rotate 360º",
+                      style: TextTheme.of(
+                        context,
+                      ).bodyMedium?.copyWith(fontFamily: "Poppins"),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Text(
+                      "As you rotate, the flower gains color again to show you your progress while rotating",
+                      style: TextTheme.of(
+                        context,
+                      ).bodyMedium?.copyWith(fontFamily: "Poppins"),
+                    ),
+                  ),
+                  Image.asset('assets/help_modal/5th_row.png'),
+                  SizedBox(width: 12),
+                ],
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/help_modal/6th_row.png'),
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      "With the flower colored, dashes will appear in the direction the sound is coming from!",
+                      style: TextTheme.of(
+                        context,
+                      ).bodyMedium?.copyWith(fontFamily: "Poppins"),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 10)),
               Align(
                 alignment: Alignment.center,
                 child: Text(
-                  "Turn around until you find the direction of the sound",
+                  "And that's it! Then you can use the buttons bellow the flower to restart the process again",
                   style: TextTheme.of(
                     context,
                   ).bodyMedium?.copyWith(fontFamily: "Poppins"),
